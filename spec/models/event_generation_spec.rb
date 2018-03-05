9910c6518e7f4d5be3e4e15c2b9adcf70b625db8
Lucho Cannavo <luchotc@hotmail.com.ar>
2018-03-05 15:15:12 -0300

    removing content_medatada from tests

--- spec/models/event_generation_spec.rb
+++ spec/models/event_generation_spec.rb
@@ -35,7 +35,6 @@ describe '#as_platform_json' do
                            feedback: nil,
                            test_results: nil,
                            submissions_count: 2,
-                           content_metadata: nil,
                            manual_evaluation_comment: nil,
                            exercise: {
                              eid: exercise.bibliotheca_id,
@@ -94,7 +93,6 @@ describe '#as_platform_json' do
                            feedback: nil,
                            test_results: nil,
                            submissions_count: 2,
-                           content_metadata: nil,
                            manual_evaluation_comment: nil,
                            exercise: {
                              name: exercise.name,
@@ -149,7 +147,6 @@ describe '#as_platform_json' do
                            feedback: nil,
                            test_results: nil,
                            submissions_count: 2,
-                           content_metadata: nil,
                            manual_evaluation_comment: nil,
                            exercise: {
                              eid: exercise.bibliotheca_id,
