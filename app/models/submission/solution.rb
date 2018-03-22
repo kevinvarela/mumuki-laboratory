8902e3eceb9b61f427ef8de07afa36f098ba2b6d
Lucho Cannavo <luchotc@hotmail.com.ar>
2018-03-22 15:26:18 -0300

    method to expose kids results modal

--- app/models/submission/solution.rb
+++ app/models/submission/solution.rb
@@ -2,6 +2,6 @@ class Solution < PersistentSubmission
   attr_accessor :content, :content_metadata
 
   def try_evaluate_exercise!(assignment)
-    assignment.run_tests!({ content: content }).except(:response_type)
+    assignment.run_tests!(content: content).except(:response_type)
   end
 end
